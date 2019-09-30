99033ae8970aac4930320ecc165da2d23c52b826
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-09-30 20:38:10 +0300

    added task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,16 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+	"my_list" : [
+    {"restaurant_name" : "Elevation",
+    "food_type" : "burger"}, 
 
+    {"restaurant_name" : "Five Guys",
+    "food_type" : "burger"}, 
+
+    {"restaurant_name" : "Shake Shack",
+    "food_type" : "burger"}, 
+    ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +23,9 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    "my_object" : {
+    "restaurant_name": "Pizza Hut",
+    "food_type" : "pizza" 
+    }
     }
     return render(request, 'detail.html', context)
